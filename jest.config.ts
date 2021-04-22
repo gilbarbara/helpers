a3321dc63ae6dd8fa0ad8b697eb43a425bebdf7b
Gil Barbara <gilbarbara@gmail.com>
2021-04-22 15:44:48 -0300

    Add config files

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+module.exports = {
+  collectCoverage: false,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'test/tsconfig.json',
+    },
+  },
+  moduleDirectories: ['node_modules', 'src', './'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+  preset: 'ts-jest',
+  setupFiles: ['<rootDir>/test/__setup__/setupFiles.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test/__setup__/setupTests.ts'],
+  snapshotSerializers: ['jest-serializer-html'],
+  testMatch: null,
+  testRegex: '/test/.*?\\.(test|spec)\\.tsx?$',
+  testURL: 'http://localhost:1337/',
+  verbose: false,
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+};
