255c30381c0e978fa2d05dfd000b4ff4a89dceca
Gil Barbara <gilbarbara@gmail.com>
2021-09-07 19:04:59 -0300

    Add getInitials to strings

--- src/strings.ts
+++ src/strings.ts
@@ -22,6 +22,23 @@ export function cleanupURI(input: string) {
 }
 
 /**
+ * Get initials from name
+ */
+export function getInitials(input: string) {
+  if (!input) {
+    return '';
+  }
+
+  const names = input.trim().split(' ');
+
+  if (names.length === 1) {
+    return `${names[0].charAt(0)}`;
+  }
+
+  return `${names[0].charAt(0)}${names[names.length - 1].charAt(0)}`;
+}
+
+/**
  * Pluralize strings.
  *
  * If the plural form just adds an `s` to the end, you don't need to pass it.
